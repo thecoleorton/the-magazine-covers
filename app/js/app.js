dfbb77d2e3ac28461d6010aaf435b5ec209add14
Cole Orton <coleorton@gmail.co>
2013-11-11 19:31:12 -0600

    wrap the app around angular seed 1.2.0

---
+++ app/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var BASE_URL = "http://localhost:3000/" // dev
+/* var BASE_URL = "http://the-magazine-covers.org:3000/" // prod */
+
+var CURRENT_ISSUE_NUMBER_PATH = "api/current_issue_number"
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.otherwise({redirectTo: '/'});
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+	.controller('MyCtrl1', ['$http', '$scope', function( $http, $scope ) {
+		
+		$http({method: 'GET', url: BASE_URL + CURRENT_ISSUE_NUMBER_PATH})
+		.success(function(data, status, headers, config) {
+	    // this callback will be called asynchronously
+	    // when the response is available
+	
+	    console.log("success() callback");
+	    console.log(data);
+	    console.log(status);
+	    console.log(headers);
+	    console.log(config);
+	    
+			$scope.current_issue_number = data[0].issue;
+			console.log($scope.current_issue_number);
+			
+			$scope.number_of_issues_array = [];
+			for (var i = 0; i < $scope.current_issue_number; i++) {
+				$scope.number_of_issues_array.push(i);
+			}
+			
+			console.log($scope.number_of_issues_array.length);
+
+			
+	  })
+	  .error(function(data, status, headers, config) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+	    
+	    console.log("error() callback");
+	    console.log(data);
+	    console.log(status);
+	    console.log(headers);
+	    console.log(config);
+	
+	  });
+
+  }]);
