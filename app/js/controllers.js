064c15616198d3f3ef5676c5c54ff43c15dd5f53
Cole Orton <coleorton@gmail.co>
2013-11-12 11:36:31 -0600

    add angular-ui 0.6.0

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -40,5 +40,20 @@ angular.module('myApp.controllers', [])
 	    console.log(config);
 	
 	  });
+	  
+			$scope.myInterval = -1;
+			var slides = $scope.slides = [];
+			$scope.addSlide = function() {
+			var newWidth = 200 + ((slides.length + (25 * slides.length)) % 150);
+			slides.push({
+			  image: 'http://placekitten.com/' + newWidth + '/200',
+			  text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+			    ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+			});
+			};
+			for (var i=0; i<4; i++) {
+				$scope.addSlide();
+			}
+
 
   }]);
