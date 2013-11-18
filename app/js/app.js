31d102f38cab894ff31b206b3ac29549ede5e41b
Cole Orton <coleorton@gmail.com>
2013-11-17 19:14:19 -0600

    delete basic http authentication

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var BASE_URL = "https://hKvmhFzQEwFjRUFC4HlIsKpjKBEmrjZx9Z06AuNV:javascript-key=lVBaGQolJCNHlkwDr2R6y85QRtK9bLzlXZHSIw8z@api.parse.com" // prod
+var BASE_URL = "https://api.parse.com" // prod
 var CURRENT_ISSUE_NUMBER_PATH = "/1/classes/Issue" // prod
 
 // Declare app level module which depends on filters, and services
